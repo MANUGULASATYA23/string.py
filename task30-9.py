9553fa920cc2b08acc856d3ffc511fe3b84be647
MANUGULASATYA23 <145895699+MANUGULASATYA23@users.noreply.github.com>
2023-10-03 21:57:12 +0530

    Add files via upload

---
+++ task30-9.py
@@ -0,0 +1,37 @@
+#left shift operator
+a=8
+b=2
+print(8>>2)
+
+#right shift operator
+a=8
+b=2
+print(1<<2)
+
+#special operators
+a=10
+print(id(a))
+
+#is
+a=8
+b=8
+print(a is b)
+
+#is not
+a=8
+b=4
+print(a is not b)
+
+#in operator
+a="satya@gmail.com"
+print("@" in a)
+attendees=["satya","gowri","swarna"]
+name="satya"
+print(name in attendees)
+
+#not in operator
+a="satya@gmail.com"
+print("j" not in a)
+attendees=["satya","gowri","swarna"]
+name="devi"
+print(name not in attendees)
