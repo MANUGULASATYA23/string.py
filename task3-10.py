7765c37da210c19e20537a17ea2171f6aaf16e51
MANUGULASATYA23 <145895699+MANUGULASATYA23@users.noreply.github.com>
2023-10-03 22:00:22 +0530

    Add files via upload

---
+++ task3-10.py
@@ -0,0 +1,127 @@
+#mathmatical oparations
+#addition
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) + int(b))
+
+#substation
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) - int(b))
+
+#multiflication
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) * int(b))
+
+#division
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) /int(b))
+
+#modulus
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) % int(b))
+
+#floor division
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) // int(b))
+
+#assignment oparator
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) == int(b))
+
+#logical oparator
+#and
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a)<int(b) and int(a)>int(b))
+
+#or
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a)<int(b) or int(a)>int(b))
+
+#not
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(not(int(a)<int(b) and int(a)>int(b)))
+
+#comparision oparator
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) == int(b))
+
+#<=
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) <= int(b))
+
+#>=
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) >= int(b))
+
+#>
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) > int(b))
+
+#<
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) < int(b))
+
+#!=
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) != int(b))
+
+#bitwise oparators
+#and
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) & int(b))
+
+#or
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) | int(b))
+
+#xor
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) ^ int(b))
+
+#leftshift
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) >> int(b))
+
+#rightshift
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) << int(b))
+
+#identity oparator
+#is
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) is int(b))
+
+#is not
+a = input("enter a value: ")
+b = input("enter b value: ")
+print(int(a) is not int(b))
+
+#member ship oparator
+#in
+a = input("enter a Email: ")
+print("@" in a)
+
+#not in
+a = input("enter a gmail: ")
+print("@" not in a)
