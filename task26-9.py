f492766339b7ec831b183850fab436ed843fc29f
MANUGULASATYA23 <145895699+MANUGULASATYA23@users.noreply.github.com>
2023-10-03 21:54:30 +0530

    Add files via upload

---
+++ task26-9.py
@@ -0,0 +1,110 @@
+#string transform function
+#capitalize
+a = "hi how are you"
+print(a.capitalize())
+
+#title
+a = "hi how are you"
+print(a.title())
+
+#upper
+a = "hi how are you"
+print(a.upper())
+
+#lower
+a = "SATYA"
+print(a.lower())
+
+#casefold
+a = "SATYA"
+print(a.casefold())
+
+#swapcase
+a = "satya"
+print(a.swapcase())
+
+
+#string check function
+#isnumeric
+a="123"
+print(a.isnumeric())
+
+#isalnumeric
+a="satya123"
+print(a.isalnum())
+
+#isdecimal
+a="45.667"
+b="123"
+print(a.isdecimal())
+print(b.isdecimal())
+
+#isdigit
+a="1244"
+b="24.67"
+print(a.isdigit())
+print(b.isdigit())
+
+#isasscii
+a="satya"
+b="1233"
+c="ddf557/@$#%"
+print(a.isascii())
+print(b.isascii())
+print(c.isascii())
+
+#isupper
+a="satya"
+b="SATYA"
+print(a.isupper())
+print(b.isupper())
+
+#islower
+a="satya"
+b="SATYA"
+print(a.islower())
+print(b.islower())
+
+#isspace
+a="255"
+b=" "
+c="satya 245"
+print(a.isspace())
+print(b.isspace())
+print(c.isspace())
+
+#isidentifiers
+a="satya"
+b="7678"
+c="satay565"
+d="#$#%"
+print(a.isidentifier())
+print(b.isidentifier())
+print(c.isidentifier())
+print(d.isidentifier())
+
+
+#isprintable
+a="?/=+"
+print(a.isprintable())
+
+#istitle
+a="satya swamy"
+b="Satya Swamy"
+print(a.istitle())
+print(b.istitle())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
