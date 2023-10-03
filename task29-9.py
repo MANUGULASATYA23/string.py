38cd0c5a3a4f2f464d1378254cbf386db945ab2b
MANUGULASATYA23 <145895699+MANUGULASATYA23@users.noreply.github.com>
2023-10-03 21:56:25 +0530

    Add files via upload

---
+++ task29-9.py
@@ -0,0 +1,143 @@
+#arthematic operator
+#addition
+a=10
+b=3
+print(a+b)
+
+#substraction
+a=10
+b=3
+print(a-b)
+
+#multipication
+a=10
+b=3
+print(a*b)
+
+#division
+a=9
+b=3
+print(a/b)
+
+#module
+a=10
+b=3
+print(a%b)
+
+#floor division
+a=10
+b=3
+print(a//b)
+
+
+#assignment operators
+#equal to
+a=10
+a=a+2
+print(a)
+
+#addition equal to
+a=10
+a=a+2
+print(a)
+
+#substraction equal to
+a=10
+a-=2
+print(a)
+
+#multification equal to
+a=10
+a=a*2
+print(a)
+
+#module equal to
+a=9
+a=a%2
+print(a)
+
+#division equal to
+a=9
+a/=2
+print(a)
+
+#floor division
+a=9
+a=a//2
+print(a)
+
+
+#and operator
+a=5
+b=10
+c=7
+print(a<b and a<c)
+print(a<b and a>c)
+
+#or operator
+a=5
+b=10
+c=7
+print(a<b or a<c)
+print(a<b or a>c)
+
+#not oprator
+a=5
+b=10
+c=7
+print(not(a<b and a<c))
+print(not(a<b and a>c))
+
+#comparison operator
+#double equal to
+a=10
+b=10
+print(a==b)
+
+#lessthan equal to
+a=10
+b=10
+print(a<=b)
+
+#graterthan equal to
+a=10
+b=10
+print(a>=b)
+
+
+# equal to
+a=10
+b=9
+print(a!=b)
+
+
+#lessthan
+a=6
+b=10
+print(a<b)
+
+#greaterthan
+a=18
+b=10
+print(a>b)
+
+#bitwise operator
+#and operator
+a=6
+b=2
+print(a and b)
+print(10 and 2)
+
+#or operator
+a=6
+b=2
+print(a | b)
+print(46 | 2)
+
+#xor operator
+a=6
+b=2
+print(a ^ b)
+print(10 ^ 2)
+
+
