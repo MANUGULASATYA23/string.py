0791e1aabd0a0d3de0a06bba249d5e0752be6038
MANUGULASATYA23 <145895699+MANUGULASATYA23@users.noreply.github.com>
2023-10-12 22:30:31 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,45 @@
+num = 5
+i=1
+fact =1
+while i <= num:
+    fact *= i
+    i += 1
+    print(fact)
+
+
+
+
+#swapw 2 numbers
+a=1
+b=2
+a=a+b
+b=a-b
+a=a-b
+print(a,b)
+
+#swapw 2 temp numbers
+a=2
+b=1
+temp=a
+a=b
+b=temp
+print(a,b)
+
+
+#fibonocci series
+n=10
+a=1
+b=0
+i=1
+while i <=n:
+    print(b)
+    a=a+b
+    b=a-b
+    i +=1
+
+
+
+
+
+
+
